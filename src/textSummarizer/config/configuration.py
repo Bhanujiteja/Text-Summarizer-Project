c5e95b3bfbf7465526cf1443444500e916979045
Bhanujiteja <bhanujiteja@gmail.com>
2024-12-22 12:33:55 +0530

    data validation completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('src')
 from src.textSummarizer.constants import *
 from src.textSummarizer.utils.common import read_yaml,create_directories
-from src.textSummarizer.entity import (DataIngestionConfig)
+from src.textSummarizer.entity import (DataIngestionConfig,DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -30,6 +30,19 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            ALL_REQUIRED_FILES=config.ALL_REQUIRED_FILES,
+        )
+
+        return data_validation_config
     
 class DataIngestionConfig:
     def __init__(self, root_dir, source_URL, local_data_file, unzip_dir):
